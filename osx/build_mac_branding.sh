619acde10d8e1dee4e06c039c48b3bf43885ff4d
xavi-b <developer@xavi-b.fr>
2022-03-26 00:46:59 +0100

    Signing

--- osx/build_mac_branding.sh
+++ osx/build_mac_branding.sh
@@ -22,9 +22,13 @@ mkdir -p build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../install ../desktop -DBRANDING_VALUE=$1 \
     -DBUILD_OWNCLOUD_OSX_BUNDLE=ON \
-    -DCMAKE_OSX_SYSROOT="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.14.sdk/" \
-    -DCMAKE_OSX_DEPLOYMENT_TARGET=10.14
+    -DCMAKE_OSX_SYSROOT="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.10.sdk/" \
+    -DCMAKE_OSX_DEPLOYMENT_TARGET=10.10
 make
 make install
-./admin/osx/create_mac.sh ../install . $2
+
+codesign -s 'Developer ID Application: LEVIIA (7S955PF2T8)' --timestamp --options=runtime --force --preserve-metadata=entitlements --verbose=4 --deep install/Leviia.app
+
+./admin/osx/create_mac.sh ../install . "Developer ID Installer: LEVIIA (7S955PF2T8)"
+
 cd -
